cfc4f73b2d7398ce6429597a0b4fec07d34dc604
Mo'men Almohtaseb <159964099+Mo7taseb@users.noreply.github.com>
2026-05-23 18:49:01 +0300

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        if(strs.size() == 0) return null;
+        StringBuilder res = new StringBuilder();
+        for (int i =0 ; i < strs.size(); i++){
+            if (strs.get(i).length() == 0) {
+                res.append("-");
+            }
+            res.append(strs.get(i) + "#amr");
+        }
+    return res.toString();
+    }
+
+    public List<String> decode(String str) {
+        if (str == null) return new ArrayList<>();
+        ArrayList<String> res = new ArrayList<>(Arrays.asList(str.split("#amr")));
+        for(int i = 0; i < res.size(); i++) {
+            if (res.get(i).length() == 1 && res.get(i).charAt(0) == '-') {
+                res.set(i, "");
+            }
+        }
+        return res;
+
+    }
+}
