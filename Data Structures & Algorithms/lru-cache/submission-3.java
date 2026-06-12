0845e63549cc01d5868d49306cc3feebc6dc6467
Mo'men Almohtaseb <159964099+Mo7taseb@users.noreply.github.com>
2026-06-13 02:27:10 +0300

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.java
@@ -0,0 +1,70 @@
+class Node {
+    int key;
+    int value;
+    Node prev;
+    Node next;
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class LRUCache {
+    int capacity;
+    HashMap<Integer, Node> map;
+    private Node head;
+    private Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    private void remove(Node curr) {
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+    }
+    private void insert(Node curr) {
+        Node last = tail.prev;
+
+        last.next = curr;
+        curr.next = tail;
+        curr.prev = last;
+        tail.prev = curr;
+    }
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node curr = map.get(key);
+            remove(curr);
+            insert(curr);
+            return curr.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.get(key).value = value;
+            Node curr = map.get(key);
+            remove(curr);
+            insert(curr);
+        } else if (capacity > 0) {
+            Node curr = new Node(key, value);
+            insert(curr);
+            map.put(key, curr);
+            capacity--;
+        } else {
+            map.remove(head.next.key);
+            remove(head.next);
+            Node curr = new Node(key, value);
+            insert(curr);
+            map.put(key, curr);
+        }
+    }
+}
